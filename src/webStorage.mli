0b98a15a7562163d754932971193111f04844a6b
xvw <xavier.vdw@gmail.com>
2017-05-15 17:06:15 +0200

    Append signature for Storage

--- src/webStorage.mli
+++ src/webStorage.mli
@@ -50,111 +50,46 @@ end
 type event = storageEvent Js.t
 val event : event Dom.Event.typ
 
-(** {1 Types} *)
+(** {1 Storage} *)
 
 (** Shortcut for [Dom_html.storage Js.t] *)
 type t = Dom_html.storage Js.t
 
-(*
-
-(** Shortcut for events *)
-type event = storageEvent Js.t
-
-(** An event to be used with [addEventListener] *)
-val event:  event Dom.Event.typ
-
-(** An event to be used with [Lwt_js_events] *)
-val lwt_js_event: 
-  ?use_capture:bool 
-  -> Dom_html.window Js.t
-  -> event Lwt.t
-
 (** {1 Interfaces} *)
 
-module type STORAGE =
+(** The basic interface of a storage handler *)
+module type STORAGE = 
 sig 
 
-  type key = string
+  type key = string 
   type value = string
+  type old_value = string
+  type url = string
 
-  (** Event received where an update is performing on the storage *)
-  type storageEvent = {
-    key: key option
-  ; old_value: value option
-  ; new_value: value option
-  ; url: string
-  }
+  type change_state = 
+    | Clear
+    | Insert of key * value 
+    | Remove of key * old_value 
+    | Update of key * old_value * value
 
-  (** Returns the JavaScript object of the handler *)
+
+  val is_supported: unit -> bool
   val handler: t
-
-
-  (** Returns an integer representing the number of data items stored 
-      in the Storage object. 
-  *)
   val length: unit -> int
-
-
   val get: key -> value option
-  (** When passed a key name, will return that key's value. *)
-
-  (** When passed a key name and value, will add that key to 
-      the storage, or update that key's value if it already exists. 
-  *)
   val set: key -> value -> unit
-
-  (** When passed a key name, will remove that key from the storage. *)
   val remove: key -> unit 
-
-  (** When invoked, will empty all keys out of the storage. *)
   val clear: unit -> unit
-
-  (** When passed a number n, this method will return the name of the 
-      nth key in the storage. 
-  *)
   val key: int -> key option
-
-  (** Get the couple Key/Values with a numeric index *)
   val at: int -> (key * value) option
-
-  (** Returns all of a Storage as an [Hashtbl.t] *)
   val to_hashtbl: unit -> (key, value) Hashtbl.t
-
-  (** [iter (fun key value -> ... )] apply f on each row of the Storage *)
   val iter: (key -> value -> unit) -> unit
-
-  (** [find p] returns the first element of the storage that
-      satisfies the predicate [p]. *)
   val find: (key -> value -> bool) -> (key * value) option
-
-  (** [select p] returns all the elements of storage
-      that satisfy the predicate [p] in an [Hashtbl.t]. *)
   val select: (key -> value -> bool) -> (key, value) Hashtbl.t
 
-  (** Watch the mutation of a storage *)
-  val onchange: ?prefix:string -> (storageEvent -> unit) -> Dom.event_listener_id
-
-  (** Watch if a new value is inserted *)
-  val oninsert: ?prefix:string -> (key -> value -> unit) -> Dom.event_listener_id
-
-  (** Watch if a value is removed *)
-  val onremove: ?prefix:string -> (key -> unit) -> Dom.event_listener_id
-
-  (** Watch if a value is updated *)
-  val onupdate: ?prefix:string -> (key -> value -> unit) -> Dom.event_listener_id
-
-  (** Watch if the storage is clear *)
-  val onclear: (unit -> unit) -> Dom.event_listener_id
+  val on_change : 
+    ?prefix:string 
+    -> (change_state -> url -> unit) 
+    -> Dom.event_listener_id
 
 end
-
-
-(** {1 Modules} *)
-
-(** Wrapper for the LocalStorage *)
-module Local : STORAGE
-
-
-(** Wrapper for the SessionStorage *)
-module Session : STORAGE
-*)
